390c3dd998b653f7e74d262eaf2422fc52800177
Aleix Vidal <al3ix.vidal@gmail.com>
2023-03-02 12:21:57 +0100

    feat: add some random custom colors

--- src/views/Heart/index.tsx
+++ src/views/Heart/index.tsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
 
-export const Heart = () => {
-  return <>Hello Heart</>;
+const options: Highcharts.Options = {
+  title: {
+    text: "My chart",
+    style: { color: "white" },
+  },
+  series: [
+    {
+      type: "line",
+      data: [1, 2, 3],
+    },
+  ],
+  chart: {
+    backgroundColor: "black",
+    zooming: {
+      type: "xy",
+      resetButton: {
+        position: {
+          align: "right",
+        },
+      },
+    },
+  },
+};
+
+export const Heart = (props: HighchartsReact.Props) => {
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+  return (
+    <>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        ref={chartComponentRef}
+        {...props}
+      />
+    </>
+  );
 };
